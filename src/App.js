0253a76319135f65de87bd3b7bc6c75e6817387e
USERSAD\eignaten <ekaterina.ignatenko@everis.nttdata.com>
2023-03-03 16:37:54 +0000

    Pokedex - firstPage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { getPokemonData, getPokemons } from "./api";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Pokedex from "./components/Pokedex";
+import { FavoriteProvider } from "./contexts/favouritesContext";
 
+const favoritesKey = "f"
 function App() {
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+  const [favorites, setFavorites] = useState([]);
+
+  const itensPerPage = 25;
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true);
+      const data = await getPokemons(itensPerPage, itensPerPage * page);
+      const promises = data.results.map(async (pokemon) => {
+        return await getPokemonData(pokemon.url);
+      });
+
+      const results = await Promise.all(promises);
+      setPokemons(results);
+      setLoading(false);
+      setTotalPages(Math.ceil(data.count / itensPerPage));
+    } catch (error) {
+      console.log("fetchPokemons error: ", error);
+    }
+  };
+
+  const loadFavoritePokemons = () => {
+    const pokemons = JSON.parse(window.localStorage.getItem(favoritesKey)) || []
+    setFavorites(pokemons)
+  }
+
+  useEffect(() => {
+    loadFavoritePokemons()
+  }, []);
+  
+  useEffect(() => {
+    fetchPokemons();
+  }, [page]);
+
+  const updateFavoritePokemons = (name) => {
+    const updatedFavorites = [...favorites]
+    const favoriteIndex = favorites.indexOf(name)
+    if(favoriteIndex >= 0) {
+      updatedFavorites.splice(favoriteIndex, 1);
+    }else {
+      updatedFavorites.push(name);
+    }
+    window.localStorage.setItem(favoritesKey, JSON.stringify(updatedFavorites))
+    setFavorites(updatedFavorites);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FavoriteProvider
+      value={{
+        favoritePokemons: favorites,
+        updateFavoritePokemons: updateFavoritePokemons,
+      }}
+    >
+      <div>
+        <Navbar />
+        <Pokedex
+          pokemons={pokemons}
+          loading={loading}
+          page={page}
+          setPage={setPage}
+          totalPages={totalPages}
+        />
+      </div>
+    </FavoriteProvider>
   );
 }
 
